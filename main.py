8281cc838d2fd66c6e4bd3afc5f14c69cfd56361
Alja Čekada <alja.cekada@gmail.com>
2022-11-19 18:25:52 +0100

    created login page

--- main.py
+++ main.py
@@ -1,37 +1,76 @@
 import random
+import os
 
-from flask import Flask, render_template, request, make_response
-
+from flask import Flask, render_template, request, make_response, redirect, url_for
+from sqla_wrapper import SQLAlchemy
 
 app = Flask(__name__)
+db_url = os.getenv("DATABASE_URL", "sqlite:///db.sqlite").replace("postgres://", "postgresql://", 1)
+db = SQLAlchemy(db_url)
+db.create_all()
+
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String, unique=True)
+    email = db.Column(db.String, unique=True)
+    secret_num = db.Column(db.Integer, unique=False)
 
 
 @app.route("/", methods=["GET"])
 def index():
-    secret_num = request.cookies.get("secret_num")  # check if there is a cookie
+    email_address = request.cookies.get("email")  # check if there is a cookie with user
+    if email_address:  # get user from the database based on email address
+        user = db.query(User).filter_by(email=email_address).first()
+    else:
+        user = None
 
-    response = make_response(render_template("index.html"))
-    if not secret_num:  # if there is no cookie, create a new one
-        new_secret_num = random.randint(1, 30)
-        response.set_cookie("secret_num", str(new_secret_num))
+    return render_template("index.html", user=user)
+
+
+@app.route("/login", methods=["POST"])
+def login():
+    name = request.form.get("user-name")
+    email = request.form.get("user-email")
+    secret_num = int(random.randint(1, 30))
+
+    # see if user already exists
+    user = db.query(User).filter_by(email=email).first()
+
+    # create a User object
+    if not user:
+        user = User(name=name, email=email, secret_num=secret_num)
+        user.save()  # save user into the database
+
+    # save user's email into a cookie
+    response = make_response(redirect(url_for('index')))
+    response.set_cookie("email", email)
+    response.set_cookie("name", name)
 
     return response
 
 
 @app.route("/result", methods=["POST"])
 def result():
-    secret_num = int(request.cookies.get("secret_num"))
     guess = int(request.form.get("guess"))
 
-    if guess == secret_num:
+    email_address = request.cookies.get("email")
+
+    # get user from the database based on her/his email address
+    user = db.query(User).filter_by(email=email_address).first()
+
+    if guess == User.secret_num:
         message = f"Congratulations {guess} was the secret number!"
         response = make_response(render_template("win.html", message=message))
-        response.set_cookie("secret_num", expires=0)
+
+        new_secret = random.randint(1, 30)  # create a new random secret number
+        user.secret_number = new_secret
+        user.save()
         return response
-    elif guess < secret_num:
+    elif guess < User.secret_num:
         message = f"try a number that's bigger than {guess}"
         return render_template("result.html", message=message)
-    elif guess > secret_num:
+    elif guess > User.secret_num:
         message = f"try a number that's smaller than {guess}"
         return render_template("result.html", message=message)
 
